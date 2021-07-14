aa9b1e1e3d54c8c128dbf07b7683d5cb51181be0
hzz <dexter.ho.cn@gmail.com>
2021-07-14 17:51:45 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-ip2region
+
+go 1.15
+
+require (
+	github.com/lionsoul2014/ip2region v2.2.0-release+incompatible
+	github.com/thinkeridea/go-extend v1.3.2
+)
---
+++ main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"github.com/lionsoul2014/ip2region/binding/golang/ip2region"
+	"github.com/thinkeridea/go-extend/exnet"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+var (
+	wg   = sync.WaitGroup{}
+	port = ""
+	d    = "" // 下载标识
+)
+
+const ipDbPath = "./ip2region.db"
+
+type JsonRes struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+type IpInfo struct {
+	Ip       string `json:"ip"`
+	Country  string `json:"country"`  // 国家
+	Province string `json:"province"` // 省
+	City     string `json:"city"`     // 市
+	County   string `json:"county"`   // 县、区
+	ISP      string `json:"isp"`      // 互联网服务提供商
+}
+
+func init() {
+	checkIpDbIsExist()
+
+	_p := flag.String("p", "9090", "本地监听的端口")
+	_d := flag.String("d", "0", "仅用于下载最新的ip地址库，保存在当前目录")
+	flag.Parse()
+
+	port = *_p
+	d = *_d
+
+	if d == "1" {
+		downloadIpDb()
+		os.Exit(1)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", queryIp)
+
+	link := "http://127.0.0.1:" + port
+
+	log.Println("监听端口", link)
+	listenErr := http.ListenAndServe(":"+port, nil)
+	if listenErr != nil {
+		log.Fatal("ListenAndServe: ", listenErr)
+	}
+}
+
+func queryIp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/json")
+
+	defer func() {
+		//捕获 panic
+		if err := recover(); err != nil {
+			log.Println("查询ip发生错误", err)
+		}
+	}()
+
+	r.ParseForm() // 解析参数
+
+	ip := r.FormValue("ip")
+
+	if ip == "" {
+		// 获取当前客户端 IP
+		ip = getIp(r)
+	}
+
+	region, err := ip2region.New(ipDbPath)
+	defer region.Close()
+
+	if err != nil {
+		msg, _ := json.Marshal(&JsonRes{Code: 4001, Msg: err.Error()})
+		w.Write(msg)
+		return
+	}
+
+	info, searchErr := region.BinarySearch(ip)
+
+	if searchErr != nil {
+		msg, _ := json.Marshal(JsonRes{Code: 4002, Msg: searchErr.Error()})
+		w.Write(msg)
+		return
+	}
+
+	// 赋值查询结果
+	ipInfo := &IpInfo{
+		Ip:       ip,
+		ISP:      info.ISP,
+		Country:  info.Country,
+		Province: info.Province,
+		City:     info.City,
+		County:   info.Region,
+	}
+
+	msg, _ := json.Marshal(JsonRes{Code: 200, Data: ipInfo})
+	w.Write(msg)
+	return
+}
+
+func getIp(r *http.Request) string {
+	ip := exnet.ClientPublicIP(r)
+	if ip == "" {
+		ip = exnet.ClientIP(r)
+	}
+	return ip
+}
+
+func checkIpDbIsExist() {
+	if _, err := os.Stat(ipDbPath); os.IsNotExist(err) {
+		log.Println("ip 地址库文件不存在")
+		downloadIpDb()
+	}
+}
+
+func downloadIpDb() {
+	log.Println("正在下载最新的 ip 地址库...: ")
+	wg.Add(1)
+	go func() {
+		downloadFile(ipDbPath, "https://hub.fastgit.org/lionsoul2014/ip2region/raw/master/data/ip2region.db")
+		wg.Done()
+	}()
+	wg.Wait()
+	log.Println("下载完成")
+}
+
+// @link https://studygolang.com/articles/26441
+func downloadFile(filepath string, url string) error {
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
